5661de88d16b74649c6f3bf2aaf474d349cc394d
amanilester <118773252+amanilester@users.noreply.github.com>
2025-12-28 13:00:06 -0500

    did some work

--- fitness-app/src/Components/NewProgram.tsx
+++ fitness-app/src/Components/NewProgram.tsx
@@ -1,15 +1,66 @@
+import React, { useEffect, useRef, useState } from 'react';
 
-function newProgram() {
-        let programTitle = 'Select Program Title'
+function NewProgram() {
+        const [title, setTitle] = useState("New Program Title");
+        const [flag, setFlag] = useState(false);
+        const [exerciseFlag, setExerciseFlag] = useState(false);
+       
+
+        const toggleInput = () => {
+            setFlag(!flag);
+        }
+
+        const toggleExerciseInput = () => {
+            setExerciseFlag(!exerciseFlag);
+        }
+        const handleKeyDown = (e : React.KeyboardEvent<HTMLInputElement>) => {
+            if (e.key === "Enter") {
+            e.preventDefault();      // prevent form submit
+            setFlag(false);    // save + exit edit mode
+            }
+        };
+
     return (
         <div>
             <form className="m-12 bg-neutral-900 rounded-2xl">
-                <div className="flex justify-center">
-                    <h1>{programTitle}</h1>
+                <div className="flex justify-center gap-4">
+                    {flag ? <input type="text"
+                    className="m-2 bg-neutral-800 rounded-2xl text-center"
+                    value={title} onChange={(e) => setTitle(e.target.value)}
+                    onKeyDown={handleKeyDown} />
+                    :
+                    <h3 className="text-center p-2 m-2">{title}</h3>}
+                    <button type="button" onClick={toggleInput}
+                    className="ml-4 bg-sky-600 hover:bg-sky-700 m-2">
+                        {flag ? "Save" : "Edit"}
+                    </button>
                 </div>
+                
+                {!exerciseFlag ?
+                    <div className="flex justify-center">
+                        <button type="button" onClick={toggleExerciseInput} className="bg-sky-600 hover:bg-sky-700 m-2">Add Exercise</button>
+                    </div>
+                :
+                    <div className="grid grid-cols-3 gap-4 m-4 text-center">
+                        <p>Exercise Name</p>
+                        <p>Sets</p>
+                        <p >Rep Range</p>
+                        
+                        <input type="text" className="border-none bg-neutral-700 rounded-2xl text-center"/>
+                        <input type="text" className="bg-neutral-700 rounded-2xl text-center"/>
+                        <div className="flex justify-center col-span-3">
+                            <input type="text" className="bg-neutral-700 rounded-2xl text-center"/>
+                            <input type="text" className="bg-neutral-700 rounded-2xl text-center"/>
+                        </div>
+                        <div className="flex justify-center col-span-3">
+                            <button type="button" onClick={toggleExerciseInput} className="bg-black hover:bg-green-700">Done</button>
+                        </div>
+                    </div>
+                    
+                    }
             </form>
         </div>
     )
 }
 
-export default newProgram;
+export default NewProgram;
